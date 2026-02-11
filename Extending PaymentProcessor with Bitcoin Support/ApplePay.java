ba3e96f6e2bb49954ee4790b8fe95da27c210aa1
Samillah <samillah.mutoni@gmail.com>
2026-02-11 07:25:18 +0200

    Extending PaymentProcessor with Bitcoin Support
    The CreditCard, PayPal, Bitcoin, and ApplePay classes exist only to serve as parameter types for method overloading in the PaymentProcessor class.

---
+++ Extending PaymentProcessor with Bitcoin Support/ApplePay.java
@@ -0,0 +1,2 @@
+public class ApplePay {
+}
---
+++ Extending PaymentProcessor with Bitcoin Support/PaymentProcessor.java
@@ -0,0 +1,18 @@
+public class PaymentProcessor {
+    
+    public void processPayment(CreditCard card) {
+        System.out.println("Processing Credit Card Payment");
+    }
+    
+    public void processPayment(PayPal paypal) {
+        System.out.println("Processing PayPal Payment");
+    }
+    
+    public void processPayment(Bitcoin bitcoin) {
+        System.out.println("Processing Bitcoin Payment, Transaction ID: " + bitcoin.getTransactionId());
+    }
+    
+    public void processPayment(ApplePay applePay) {
+        System.out.println("Processing Apple Pay Payment");
+    }
+}
