9bdc2afc3b73b38c4c0861f36b21d78496e6ecd4
Samillah <samillah.mutoni@gmail.com>
2026-02-16 14:39:42 +0200

    Introduce factory method for creating Pair instances
    Added a factory method to create a new Pair instance.

--- Coding Task Pair K, V Key-Value Utility/src/Main.java
+++ Coding Task Pair K, V Key-Value Utility/src/Main.java
@@ -3,9 +3,14 @@ public class Main {
         Pair<String, Integer> pair1 = new Pair<>("Age", 25);
         Pair<Integer, String> pair2 = new Pair<>(404, "Not Found");
 
+        // Using factory method
+        Pair<String, Double> pair3 = Pair.create("Price", 19.99);
+
         System.out.println("Pair 1: " + pair1);
         System.out.println("Pair 2: " + pair2);
+        System.out.println("Pair 3: " + pair3);
         
     }
 
 }
+
